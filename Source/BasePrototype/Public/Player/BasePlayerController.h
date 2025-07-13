e79407e52a7470e9fec7d0b1910502b12e0ede00
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2025-07-12 21:20:08 -0400

    Create Project and Add Assets

---
+++ Source/BasePrototype/Public/Player/BasePlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEPROTOTYPE_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
