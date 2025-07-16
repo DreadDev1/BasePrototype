593b8217164176911ecb061547656d87b8513105
DreadDev1 <kameen.avery@gmail.com>
2025-07-16 14:18:06 -0400

    Add Interact to ThirdPersonController

--- Source/BasePrototype/Public/Player/ThirdPersonController.h
+++ Source/BasePrototype/Public/Player/ThirdPersonController.h
@@ -13,4 +13,10 @@ UCLASS()
 class BASEPROTOTYPE_API AThirdPersonController : public ABasePlayerController
 {
 	GENERATED_BODY()
+	virtual void SetupInputComponent() override;
+
+private:
+	void Interact();
+	UPROPERTY(EditDefaultsOnly, Category = "Base Prototype|Inputs|Input Actions")
+	TObjectPtr<UInputAction> InteractAction;
 };
